17acdf85be0f28b3754ec5987ede959d1bb05b51
Stephen Roman <sjroman12@gmail.com>
2017-07-29 21:40:32 -0400

    Further improve look and feel, add sidebar link and Section gen.

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,36 @@ var express = require('express');
 var router = express.Router();
 var tf = require('../libs/TextFill');
 
+var links = [];
+for (var i = 0; i < 20; i++ )
+  links[i] = tf.title(randInt(2, 3));
+
+var headers = [];
+for (var j = 0; j < 4; j++)
+  headers[j] = tf.title(2);
+
+var content = {};
+for (var k = 0; k < 8; k++) {
+    var prop = 'p' + k;
+    content[prop] = tf.fill(60);
+}
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: "A generated website",
-      text: tf.fill(40),
-      subtitle: tf.title(7),
-      sidebarLinks: ["foo", "bar", "baz", "you", "me", "hello"]
+      introText: tf.fill(60),
+      content: content,
+      subtitle: tf.title(6),
+      sidebarLinks: links,
+      anchorLinks: ["Foo", "Bar", "Me", "You"],
+      sectionHeaders: headers
   });
 });
+
+function randInt(start, end) {
+    return Math.floor(Math.random() * end) + start;
+}
 
 module.exports = router;
