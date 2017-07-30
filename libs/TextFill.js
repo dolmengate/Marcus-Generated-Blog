a98821151815a5b69ddfb999a503f708b343521b
Stephen Roman <sjroman12@gmail.com>
2017-07-29 21:35:25 -0400

    Improve TextFill logic

--- libs/TextFill.js
+++ libs/TextFill.js
@@ -2,42 +2,38 @@ const fs = require('fs');
 var exports = module.exports = {};
 
 var lines = fs.readFileSync(process.env.HOME + '/sites/me.com/libs/marcus.txt').toString().split("\n");
-var words = [];
 
-// get unmodified text
+// get unmodified text of a given number from a given starting point
 exports.fill = function (numWords, start) {
     if (start === undefined)
         start = 0;
     return getWords(numWords).split(' ').splice(start).join(' ');
 };
 
-// get capitalized words
+// get capitalized words stripped of punctuation
 exports.title = function (numWords) {
     var lowercase = /[a-z]/;
-    var words = getWords(numWords).split(' ');
-    for (var w = 0; w < words.length - 1; w++) {
-        var upperedWord = words[w];
-        if (lowercase.test(words[w].charAt(0))) {
-            upperedWord = words[w].charAt(0).toUpperCase();
-            for (var l = 1; l < words[w].length; l++) {
-                upperedWord += words[w].charAt(l);
+    var wordsArr = getWords(numWords).split(' ');
+    for (var w = 0; w < wordsArr.length; w++) {
+        var upperedWord = wordsArr[w];
+        if (lowercase.test(wordsArr[w].charAt(0))) {
+            upperedWord = wordsArr[w].charAt(0).toUpperCase();
+            for (var l = 1; l < wordsArr[w].length; l++) {
+                upperedWord += wordsArr[w].charAt(l);
             }
         }
         var punctuation = /[.,:;]/;
         if (punctuation.test(upperedWord.charAt(upperedWord.length - 1)))
             upperedWord = upperedWord.slice(0, upperedWord.length - 1);
-        words[w] = upperedWord;
+        wordsArr[w] = upperedWord;
     }
-    return words.join(' ');
+    return wordsArr.join(' ');
 };
 
-// get a random line and split it into words
+// get a random line and split it into a specified number of words
 function getWords(numWords) {
-    while (words.length < numWords)
-        words = lines[Math.floor(Math.random() * lines.length)].split(' ');
-    var r = '';
-    for (var i = 0; i < numWords; i++) {
-        r += words[i] + " ";
-    }
-    return r;
+    var line = '';
+    while (line.split(' ').length < numWords)
+        line = lines[Math.floor(Math.random() * lines.length)];
+    return line.split(' ').slice(0, numWords).join(' ');
 }
